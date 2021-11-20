b691f9d0ca5ef62cffc2f90e91597ec0c5e0ebfd
abhishek <abhishek.sharma_cs20@gla.ac.in>
2021-11-20 22:05:26 +0530

    Initial commit

---
+++ src/WrapperAssignment_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WrapperAssignment_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        //Using Command line argument and store the wrapper Class
+        Integer a= sc.nextInt();
+        //Print the Values
+        System.out.println("Binary Equivalant: "+Integer.toBinaryString(a));
+        System.out.println("\nHexa Equivalant: "+Integer.toHexString(a));
+        System.out.println("\nOctal Equivalant: "+Integer.toOctalString(a));
+
+    }
+}
